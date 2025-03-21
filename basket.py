fba796baaa58e8f9ba2315b6f49c05fd4e8c62ce
GeonWoo Im <gunwoo14789@naver.com>
2025-03-21 22:30:43 +0900

    basket second homewort

--- basket.py
+++ basket.py
@@ -1,80 +1,67 @@
 class Basket:
-    def __init__(self,name:str):
+    def __init__(self, name: str):
         self.name = name
         self.arr = []
         self.add_check = 0
 
-    def add_item(self,value:str):
+    def add_item(self, value: str):
         self.arr.append(value)
-
 
     def __len__(self):
         return len(self.arr)
 
+    def __getitem__(self, key):
+        return self.arr[key]
 
-    def __getitem__(self,key):
-        return self.arr[key]   
-
-
-    def __setitem__(self,key,value):
+    def __setitem__(self, key, value):
         self.arr[key] = value
-
 
     def __iter__(self):
         return iter(self.arr)
 
-
-    def __add__(self,other):
-        add_name = f"{self.name}과 {other.name}"
-        new_basket = Basket(add_name)
-        new_basket.arr = self.arr+other.arr
-        new_basket.add_check = 1
-        return new_basket
-    
-    def __eq__(self,other):
-        if self.arr == other.arr:
-            return True
+    def __add__(self, other):
+        if self.add_check == 0 and other.add_check == 0:
+            add_name = f"{self.name}과 {other.name}"
         else:
-            return False
-        
+            add_name = self.name
+        new_basket = Basket(add_name)
+        new_basket.arr = self.arr + other.arr
+        new_basket.add_check = self.add_check + other.add_check + 1
+        return new_basket
+
+
+    def __eq__(self, other):
+        return self.arr == other.arr
+
     def __enter__(self):
         return self
-    
 
-    def __exit__(self,exc_type,exc_value,traceback):
+    def __exit__(self, exc_type, exc_value, traceback):
         print("장바구니 사용 종료!")
-        return self       
-        
-    
-    
-    
+        return self
 
     def __str__(self):
         if self.add_check == 0:
-            return str(f"{self.name}의 장바구니: {self.arr}")    
+            return f"{self.name}의 장바구니: {self.arr}"
+        elif self.add_check == 1:
+            return f"{self.name}의 합친 장바구니: {self.arr}"
         else:
-            return str(f"{self.name}의 합친 장바구니: {self.arr}")
+            return f"{self.name} 외 {self.add_check}명의 합친 장바구니: {self.arr}"
+
 
 
 basket1 = Basket("홍길동")
 basket1.add_item("사과")
-basket1.add_item("바나")
-print(basket1) 
-print(len(basket1))
-print(basket1[0])
-basket1[1] = "딸기"
-print(basket1)
-
-for item in basket1:
-    print(item)
+basket1.add_item("바나나")
 
 basket2 = Basket("김철수")
 basket2.add_item("딸기")
 
 basket3 = basket1 + basket2
-print(basket3)
+basket4 = Basket("임건우")
+basket4.add_item("복숭아")
 
-print(basket1 == basket2)
+sum_basket = basket4 + basket3 
+sum_basket += basket1     
 
-with basket1:
-    print("장바구니 사용중")
+print(sum_basket)    
